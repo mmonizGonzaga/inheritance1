68cb46da2cd4b392cb3d95c6175e4928f1c2d7eb
Matt <kayleemoniz1@makaylas-macbook-pro.local>
2018-09-13 10:12:01 -0700

    Initial Commit

---
+++ inheritance1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  inheritance1
+//
+//  Created by Makayla Moniz on 9/13/18.
+//  Copyright © 2018 Makayla Moniz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
